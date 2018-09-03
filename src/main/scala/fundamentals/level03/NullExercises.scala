eb892f99c750ce007c76aaf48e73a4d288da731d
Leon Xiang <leon.xiang@rea-group.com>
2018-09-03 13:27:33 +1000

    pass null tests

--- src/main/scala/fundamentals/level03/NullExercises.scala
+++ src/main/scala/fundamentals/level03/NullExercises.scala
@@ -32,10 +32,10 @@ object NullExercises {
     **/
   def mkTrafficLightOrNull(str: String): TrafficLight =
     str match {
-      case "red" => ???
-      case "yellow" => ???
-      case "green" => ???
-      case _ => ???
+      case "red" => Red
+      case "yellow" => Yellow
+      case "green" => Green
+      case _ => null
     }
 
   /**
@@ -55,7 +55,12 @@ object NullExercises {
     *
     * Hint: Use `mkTrafficLightOrNull` and pattern matching
     */
-  def mkTrafficLightOrNullThenShow(str: String): String = ???
+  def mkTrafficLightOrNullThenShow(str: String): String = mkTrafficLightOrNull(str) match {
+    case Red => "Traffic light is red"
+    case Yellow => "Traffic light is yellow"
+    case Green => "Traffic light is green"
+    case _ => "Traffic light is invalid"
+  }
 
   /**
     * Write a function that converts values from the real world into a `Person`.
@@ -74,7 +79,12 @@ object NullExercises {
     * scala> mkPersonOrNull("Bob", -1)
     * = null
     **/
-  def mkPersonOrNull(name: String, age: Int): Person = ???
+  def mkPersonOrNull(name: String, age: Int): Person = {
+    if (name.isEmpty || age < 0)
+      null
+    else
+      Person(name, age)
+  }
 
   /**
     * scala> mkPersonOrNullThenChangeName("Bob", 20, "John")
@@ -88,7 +98,13 @@ object NullExercises {
     *
     * Hint: Use `mkPersonOrNull` and `changeName` (already implemented below)
     **/
-  def mkPersonOrNullThenChangeName(oldName: String, age: Int, newName: String): Person = ???
+  def mkPersonOrNullThenChangeName(oldName: String, age: Int, newName: String): Person = {
+    if (newName.isEmpty) null
+    else mkPersonOrNull(oldName, age) match {
+      case null => null
+      case person => changeName(newName, person)
+    }
+  }
 
   def changeName(newName: String, person: Person): Person = person.copy(name = newName)
 
